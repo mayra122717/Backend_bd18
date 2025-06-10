7d1000ee8a6d9ca4ae4b4fd34afdf6d1fe6062d9
“Mayra” <mayramacuri122717@gmail.com>
2025-06-10 16:32:25 -0500

    Cambio de local a la nube

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
-const pool = new Pool({
+const db = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // necesario para Supabase
+  }
 });
 
-module.exports = pool;
+module.exports = db;
